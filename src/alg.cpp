47b7b7b1d62054bc0572123f54cac5230a1086e5
Матвей Головенко <91070065+twysis@users.noreply.github.com>
2022-02-27 18:20:57 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,10 +1,11 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  int count = 0;
-  for (int i = 0; i < size; i++) {
-    if (arr[i] == value){
-      count++;
+  int k = 0;
+    for (int i = 0; i < size; i++) {
+      if (arr[i] == value) {
+        k++;
+      }
     }
-  return count; // если ничего не найдено
+  return k;
 }
